c8cada3be7d0e137a254af72e0c7dd344f2fe047
janar0077 <kurmanovjanar@gmail.com>
2024-04-27 20:21:40 +0600

    Ne ponyatnye izmenenya

---
+++ ConsoleApp1/Dishes/Dish.cs
@@ -0,0 +1,24 @@
+﻿using ConsoleApp1.enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Dish
+    {
+        public string Name;
+        public decimal Price;
+        public decimal Mass;
+
+        public Dish (string name, decimal price, decimal mass)
+        {
+            Name = name;
+            Price = price;
+            Mass = mass;
+        }
+
+    }
+}
---
+++ ConsoleApp1/Dishes/DishServise.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Dishes
+{
+    internal class DishServise
+    {
+        private List<Dish> dishes;
+
+        public DishService()
+        {
+            dishes = new List<Dish>();
+        }
+
+        public void AddDish(string name, decimal price, List<Product> ingredients)
+        {
+            dishes.Add(new Dish { Name = name, Price = price, Ingredients = ingredients });
+            Console.WriteLine("Dish has been added.");
+        }
+
+        public bool RemoveDish(string name)
+        {
+            Dish dishToRemove = dishes.Find(d => d.Name == name);
+            if (dishToRemove != null)
+            {
+                dishes.Remove(dishToRemove);
+                Console.WriteLine($"Dish has been removed: {name}.");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Dish not found.");
+                return false;
+            }
+        }
+
+        public bool UpdateDish(string name, decimal newPrice, List<Product> newIngredients)
+        {
+            Dish dishToUpdate = dishes.FirstOrDefault(d => d.Name == name);
+            if (dishToUpdate != null)
+            {
+                dishToUpdate.Price = newPrice;
+                dishToUpdate.Ingredients = newIngredients;
+                Console.WriteLine("Dish has been updated.");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Dish not found.");
+                return false;
+            }
+        }
+
+        public void DisplayDishes()
+        {
+            foreach (var dish in dishes)
+            {
+                Console.WriteLine($"Dish: {dish.Name}, Price: {dish.Price:C}");
+                Console.WriteLine("Ingredients:");
+                foreach (var Product in dish.Ingredients)
+                {
+                    Console.WriteLine($"- {Product.Product.Name} ({Product.WeightInGrams}g)");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
